193d12e02e84d661a0ecf13949d7add9e555e892
Param Arora <param2k3@gmail.com>
2025-01-31 01:06:00 +0530

    add buztop link

--- components/content/projects/utils.ts
+++ components/content/projects/utils.ts
@@ -1,54 +1,54 @@
 export const projects = [
   {
-    title: "buztop - Scalable Newsletter App",
+    title: 'buztop - Scalable Newsletter App',
     description:
-      "A Scalable Newsletter Application based on a Pub/Sub architecture using Next.js and Cloudflare Queues.",
-    img: "/buztop.png", // Add an image path if available
-    href: "/projects/buztop",
-    preview: "", // Add a live preview link if available
-    github: "https://github.com/par4m/buztop", // Add GitHub link if available
-    alt: "buztop",
-    platform: "Web",
-    stack: "Next.js, Cloudflare Queues, Docker, Pub/Sub Architecture",
+      'A Scalable Newsletter Application based on a Pub/Sub architecture using Next.js, Redis, BullMQ.',
+    img: '/buztop.png', // Add an image path if available
+    href: '/projects/buztop',
+    preview: 'digest.paramarora.com', // Add a live preview link if available
+    github: 'https://github.com/par4m/buztop', // Add GitHub link if available
+    alt: 'buztop',
+    platform: 'Web',
+    stack: 'Next.js, Redis, Queues, Docker, Pub/Sub Architecture',
     delay: 0.1,
   },
   {
-    title: "AICare - Healthcare AI Assistant",
+    title: 'AICare - Healthcare AI Assistant',
     description:
-      "An AI-powered healthcare platform using RAG for efficient medical data processing, with LangChain and Pinecone for document retrieval and symptom analysis",
-    img: "/aicare.png", // Add an image path if available
-    href: "/projects/aicare",
-    preview: "", // Add a live preview link if available
-    github: "https://github.com/par4m/aicare", // Add GitHub link if available
-    alt: "AICare",
-    platform: "Web",
-    stack: "RAG, LangChain, Pinecone, Neural Networks, AI Embeddings",
+      'An AI-powered healthcare platform using RAG for efficient medical data processing, with LangChain and Pinecone for document retrieval and symptom analysis',
+    img: '/aicare.png', // Add an image path if available
+    href: '/projects/aicare',
+    preview: '', // Add a live preview link if available
+    github: 'https://github.com/par4m/aicare', // Add GitHub link if available
+    alt: 'AICare',
+    platform: 'Web',
+    stack: 'RAG, LangChain, Pinecone, Neural Networks, AI Embeddings',
     delay: 0.2,
   },
   {
-    title: "spotAI - Smart Parking",
+    title: 'spotAI - Smart Parking',
     description:
-      "A smart parking management app that uses computer vision to detect license plates and locate available parking spots in real-time. Built with PyTorch and OpenCV.",
-    img: "/spotai.png", // Add an image path if available
-    href: "/projects/spotai",
-    preview: "", // Add a live preview link if available
-    github: "https://github.com/par4m/spotai", // Add GitHub link if available
-    alt: "spotAI",
-    platform: "Web",
-    stack: "PyTorch, OpenCV, YOLOv8, EasyOCR, Next.js",
+      'A smart parking management app that uses computer vision to detect license plates and locate available parking spots in real-time. Built with PyTorch and OpenCV.',
+    img: '/spotai.png', // Add an image path if available
+    href: '/projects/spotai',
+    preview: '', // Add a live preview link if available
+    github: 'https://github.com/par4m/spotai', // Add GitHub link if available
+    alt: 'spotAI',
+    platform: 'Web',
+    stack: 'PyTorch, OpenCV, YOLOv8, EasyOCR, Next.js',
     delay: 0.3,
   },
   {
-    title: "felt - Sentiment AI",
+    title: 'felt - Sentiment AI',
     description:
-      "An AI model built with Next.js and PyTorch that takes a video as input and predicts its sentiment and emotion.",
-    img: "/felt.png", // Add an image path if available
-    href: "/projects/felt",
-    preview: "", // Add a live preview link if available
-    github: "https://github.com/par4m/felt", // Add GitHub link if available
-    alt: "felt",
-    platform: "Web",
-    stack: "PyTorch, Next.js, Multimodal AI, Sentiment Analysis",
+      'An AI model built with Next.js and PyTorch that takes a video as input and predicts its sentiment and emotion.',
+    img: '/felt.png', // Add an image path if available
+    href: '/projects/felt',
+    preview: '', // Add a live preview link if available
+    github: 'https://github.com/par4m/felt', // Add GitHub link if available
+    alt: 'felt',
+    platform: 'Web',
+    stack: 'PyTorch, Next.js, Multimodal AI, Sentiment Analysis',
     delay: 0.4,
   },
-];
+]
