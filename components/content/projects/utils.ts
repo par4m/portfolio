1e70fc0e88b595818f4f3e919b27de8a54b36af9
Param Arora <param2k3@gmail.com>
2025-02-03 13:48:16 +0530

    add go projects

--- components/content/projects/utils.ts
+++ components/content/projects/utils.ts
@@ -1,5 +1,30 @@
 export const projects = [
   {
+    title: 'goSync - Async API Template for Go',
+    description: 'fast and scalable api backend template for golang ',
+    img: '/gosync.jpeg', // Add an image path if available
+    href: '/projects/gosync',
+    preview: '', // Add a live preview link if available
+    github: 'https://github.com/par4m/gosync', // Add GitHub link if available
+    alt: 'gosync',
+    platform: 'Web',
+    stack: 'go, postgres, docker, aws, queues, s3',
+    delay: 0.1,
+  },
+  {
+    title: 'GoSwarm - A MMO Game in Go',
+    description:
+      'A real-time multiplayer game powered by Godot 4 and Golang, featuring scalable networking and async processing.',
+    img: '/goswarm.jpeg', // Add an image path if available
+    href: '/projects/goswarm',
+    preview: 'https://swarm.paramarora.com', // Live preview link
+    github: 'https://github.com/par4m/goSwarm', // GitHub repo
+    alt: 'GoSwarm',
+    platform: 'Web',
+    stack: 'godot4, golang, sqlite, sqlc, protobuf, docker',
+    delay: 0.1,
+  },
+  {
     title: 'buztop - Scalable Newsletter App',
     description:
       'A Scalable Newsletter Application based on a Pub/Sub architecture using Next.js, Redis, BullMQ.',
@@ -13,16 +38,16 @@ export const projects = [
     delay: 0.1,
   },
   {
-    title: 'AICare - Healthcare AI Assistant',
+    title: 'iCare - Healthcare AI Assistant',
     description:
-      'An AI-powered healthcare platform using RAG for efficient medical data processing, with LangChain and Pinecone for document retrieval and symptom analysis',
+      'An AI-powered healthcare platform using for efficient medical data processing for symptom analysis',
     img: '/aicare.png', // Add an image path if available
-    href: '/projects/aicare',
+    href: 'icare.paramarora.com',
     preview: '', // Add a live preview link if available
-    github: 'https://github.com/par4m/aicare', // Add GitHub link if available
-    alt: 'AICare',
+    github: 'https://github.com/par4m/iCare', // Add GitHub link if available
+    alt: 'ICare',
     platform: 'Web',
-    stack: 'RAG, LangChain, Pinecone, Neural Networks, AI Embeddings',
+    stack: 'LLM, LangChain, Pinecone, Neural Networks, AI Embeddings',
     delay: 0.2,
   },
   {
